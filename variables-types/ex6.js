4a1da9469a167db6e706e49b5655f8df9193d024
pinkjaguar007 <wardientrepreneur@outlook.com>
2026-09-01 13:43:04 +0200

    ex6.js

---
+++ variables-types/ex6.js
@@ -0,0 +1,9 @@
+var prompt = require('prompt-sync')();
+
+const a = Number(prompt("Enter a:"));
+const b = Number(prompt("Enter b:"));
+
+console.log(`a + b = ${a + b}`);
+console.log(`a - b = ${a - b}`);
+console.log(`a * b = ${a * b}`);
+console.log(`a / b = ${a / b}`);
